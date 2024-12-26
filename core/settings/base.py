1cef9c54351aa1a741c0462bad5fe2422580f408
Abdykadyrov-S <abdykadyrovsyimyk0708@gmail.com>
2024-12-26 18:08:30 +0600

    декомпозиция корневой папки проекта

--- core/settings/base.py
+++ core/settings/base.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv( BASE_DIR / '.env')
 
+IS_PROD = False
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
--- core/urls.py
+++ core/urls.py
@@ -19,23 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# drf_yasg
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="MerrigardenAPI",
-      default_version='v1',
-      description="Descriptions",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="abdykadyrovsyimyk0708@gmail.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-)
+from .yasq import urlpatterns_yasq
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -45,9 +29,8 @@ urlpatterns = [
     path('api/v1/users/', include('apps.users.urls')),
     path('api/v1/service/', include('apps.service.urls')),
 
-    # drf_yasg
-    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='api_swagger'),
-    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+urlpatterns += urlpatterns_yasq
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
